ef87ff7378aeacf11d33a14f6051aa257acc01ac
mahi-bashar-akash <mahibashar2023@gmail.com>
2025-04-08 08:59:13 +0600

    update-changes

--- resources/js/app/auth/layout/layout.vue
+++ resources/js/app/auth/layout/layout.vue
@@ -2,9 +2,9 @@
   <div class="w-full h-screen flex justify-center items-center bg-gradient-to-tr from-gray-600 to-blue-600">
     <div class="max-w-[550px] w-full mx-auto p-10 border border-gray-300 bg-gray-200">
       <div class="mb-5 flex justify-center text-[28px] font-bold">
-        <a href="javascript:void(0)" class="decoration-0 text-black inline-block">
+        <router-link :to="{name:'login'}" class="decoration-0 text-black inline-block">
           Chat App
-        </a>
+        </router-link>
       </div>
       <router-view></router-view>
     </div>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="{{asset('/images/logo-icon.svg')}}" type="image/x-icon">
-    <link rel="apple-touch-icon" href="{{asset('/images/logo-icon.svg')}}">
+    <link rel="shortcut icon" href="{{asset('/logo-icon.svg')}}" type="image/x-icon">
+    <link rel="apple-touch-icon" href="{{asset('/logo-icon.svg')}}">
     <title> Chat App </title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app/app.js')
+    @vite(['resources/css/app.css','resources/js/app/app.js'])
 </head>
 <body>
 
