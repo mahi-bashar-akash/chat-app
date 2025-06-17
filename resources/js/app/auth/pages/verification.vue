d6c2daafbfef3d31708a4ba078736073bfe3cf9b
mahi-bashar-akash <mahibashar2023@gmail.com>
2025-06-17 17:28:43 +0600

    build-and-deploy

--- resources/js/app/auth/pages/verification.vue
+++ resources/js/app/auth/pages/verification.vue
@@ -2,22 +2,22 @@
 
   <form @submit.prevent="verification()" class="w-full">
     <div class="mb-2 block w-full">
-      <label for="user-verification" class="block mb-1 w-full font-medium"> Verification Code </label>
-      <input id="user-verification" type="text" name="verification_code" v-model="formData.verification_code" placeholder="Enter your verification code" class="w-full min-h-[45px] duration-500 rounded-lg border border-gray-300 block px-5 focus-within:border-blue-600 outline-0" autocomplete="off" />
-      <div class="mt-1 text-rose-600 text-[13px]" v-if="error.verification_code"> {{error.verification_code[0]}} </div>
+      <label for="user-verification" class="block mb-1 text-center text-[14px]"> Verification Code </label>
+      <input id="user-verification" type="text" name="verification_code" v-model="formData.verification_code" placeholder="Enter your verification code" class="text-[13px] w-full border border-gray-200 min-h-[45px] max-h-[45px] px-4 outline-0 rounded-lg duration-300 ring-0 focus-within:ring-2 focus-within:ring-[rgba(3,181,98,1)]" autocomplete="off" />
+      <div class="mt-1 text-red-600 text-[12px]" v-if="error.verification_code"> {{error.verification_code[0]}} </div>
     </div>
     <div class="mb-3 block">
-      <button type="submit" class="bg-blue-600 inline-flex justify-center items-center duration-500 hover:bg-blue-800 font-medium text-white max-w-[200px] min-w-[200px] max-h-[45px] min-h-[45px] rounded-lg" v-if="!loading">
+      <button type="submit" class="cursor-pointer text-[14px] w-full block bg-[rgba(3,181,98,0.8)] duration-500 hover:bg-[rgba(3,181,98,1)] min-h-[45px] max-h-[45px] px-4 rounded-lg text-white" v-if="!loading">
         Verification
       </button>
-      <button type="button" class="bg-blue-600 inline-flex justify-center items-center duration-500 hover:bg-blue-800 font-medium text-white max-w-[200px] min-w-[200px] max-h-[45px] min-h-[45px] rounded-lg" v-if="loading">
+      <button type="button" class="cursor-pointer text-[14px] w-full block bg-[rgba(3,181,98,0.8)] duration-500 hover:bg-[rgba(3,181,98,1)] min-h-[45px] max-h-[45px] px-4 rounded-lg text-white" v-if="loading">
         <span class="inline-block size-[20px] border-2 border-transparent border-t-2 border-t-white rounded-full animate-spin"></span>
       </button>
     </div>
-    <div class="text-center font-medium">
+    <div class="w-full block text-center text-[14px]">
       Go back
-      <router-link :to="{name:'login'}" class="underline inline-block text-blue-500 duration-500 hover:text-blue-800">
-        Login
+      <router-link :to="{name:'login'}" class="decoration-0 text-[rgba(3,181,98,1)]">
+        Sign In
       </router-link>
     </div>
   </form>
